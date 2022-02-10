53741d57c28aeba4dc3c7c7063586a3650fe9ffd
Sachin Lakum <9879818230s@gmail.com>
2022-02-10 22:25:03 +0530

    Second Commit

---
+++ lib/home.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    int days = 69;
+    String name = "Sachin";
+    // double pi = 3.14;
+    // bool isMale = true;
+    // num temp = 30.5; //num takes both integer and double
+    // var day = "Thursday"; //takes ANY variable
+    // var a = 10;
+    // const pie = 3.14; //Cannot be changed
+    // final int b = 16; //Can be changed if it is a list
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("My First App"),
+      ), //Gives the app the blue bar on top
+      body: Center(
+          child: Container(
+        child: Text("Welcome to $days of flutter by $name"),
+      )),
+      drawer: Drawer(),
+    );
+  }
+}
