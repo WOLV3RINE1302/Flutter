512efa282718924163669092aa1af7544b03eb8c
Sachin Lakum <9879818230s@gmail.com>
2022-02-11 16:16:44 +0530

    Third Commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,18 +2,33 @@ import 'dart:ffi';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_catalg/pages/home.dart';
+import 'package:flutter_catalg/pages/login.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: HomePage(),
+    return MaterialApp(
+      // home: HomePage(), removed as we have home in routes(23)
+      themeMode: ThemeMode.system,
+      theme: ThemeData(
+        primarySwatch: Colors.grey,
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark, //This will ovverride colors in Swatch
+      ),
+      initialRoute: "/", //Used to set initial route
+      routes: {
+        "/": (context) =>
+            LoginPage(), //We are creating an object here but no need to use new
+        "/home": (context) => HomePage(),
+      },
     );
   }
 }
+
+// bringVegtables({required bool bag, int money = 100}) {} //{} - not compulsory 
+// // except when required it written
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+  HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -19,6 +19,7 @@ class HomePage extends StatelessWidget {
         title: const Text("My First App"),
       ), //Gives the app the blue bar on top
       body: Center(
+          // ignore: avoid_unnecessary_containers
           child: Container(
         child: Text("Welcome to $days of flutter by $name"),
       )),
